e0648595f5a2dee866d0819c0111a332d2635741
mkim <x1michelleskim@gmail.com>
2019-08-20 10:59:29 -0700

    Test movingObject function and put on the window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Webpack is working!")
+// console.log("Webpack is working!");
+
+const MovingObject = require("./moving_object.js")
+
+MovingObject.prototype.draw = function(ctx) {
+    
+}
+
+window.MovingObject = MovingObject;
