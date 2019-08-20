41fe201e109a9b8477ffea44a09f24e2626c52bc
mkim <x1michelleskim@gmail.com>
2019-08-20 11:09:08 -0700

    Draw a moving object through canvas

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@
 
 const MovingObject = require("./moving_object.js")
 
-MovingObject.prototype.draw = function(ctx) {
-    
-}
 
-window.MovingObject = MovingObject;
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById('game-canvas');
+
+    const ctx = canvas.getContext('2d');
+    
+    const mo = new MovingObject(
+        { pos: [30, 30], vel: [10, 10], radius: 5, color: "#00FF00" }
+    );
+
+    mo.draw(ctx);
+    
+    window.MovingObject = MovingObject;
+
+    console.log('webpack is working!');
+});
